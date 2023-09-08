569f6019b913672848b4bd211958f78a78be8f29
Sayyab Khan <sayyabkhan007@gmail.com>
2023-09-08 05:13:52 -0700

    Solved Exercise 1

---
+++ ForLoop/Exe_1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    internal class Exe_1
+    {
+        //1. Write a program in C# Sharp to display the first 10 natural numbers.
+
+        public static void DisplayNum()
+        {
+            for (int i =   1 ; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
