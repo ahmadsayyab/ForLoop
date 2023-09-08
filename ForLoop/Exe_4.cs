e4703e203aefe3b2ab48b5c80e3ddc2c343594fc
Sayyab Khan <sayyabkhan007@gmail.com>
2023-09-08 05:33:29 -0700

    Solved Exercise 4

---
+++ ForLoop/Exe_4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    internal class Exe_4
+    {
+        //4. Write a C# Sharp program to read 10 numbers and find their average and sum.
+
+        public static void FindSumAndAvg()
+        {
+            Console.WriteLine("Enter 10 numbers");
+            int sum = 0;
+            double avg;
+
+            for (int i = 1; i <= 10 ; i++)
+            {
+                Console.WriteLine($"Enter number {i}: ");
+                int num = int.Parse(Console.ReadLine());
+
+                sum += num;
+            }
+
+            avg = sum / 10.0;
+            Console.WriteLine($"The sum of your qo digits is: {sum}");
+            Console.WriteLine($"The average is: {avg}");
+            
+
+
+        }
+    }
+}
