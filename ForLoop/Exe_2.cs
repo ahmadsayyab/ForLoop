d751d46e6827f6266a098f4696aa231d23974e90
Sayyab Khan <sayyabkhan007@gmail.com>
2023-09-08 05:17:51 -0700

    Solved Exercise 2

---
+++ ForLoop/Exe_2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    internal class Exe_2
+    {
+        //2. Write a C# Sharp program to find the sum of the first 10 natural numbers.
+
+        public static void ShowSum()
+        {
+            int sum = 0;
+
+            for(int i = 1; i <= 10; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"The sum is: {sum}");
+        }
+    }
+}
