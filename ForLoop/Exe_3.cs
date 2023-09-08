af3da3617d3ae56a463078f7e165071939413b51
Sayyab Khan <sayyabkhan007@gmail.com>
2023-09-08 05:25:01 -0700

    Solved Exercise 3

---
+++ ForLoop/Exe_3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    internal class Exe_3
+    {
+        //3. Write a C# Sharp program that displays the sum of n natural numbers.
+
+        public static void ShowSum()
+        {
+
+            string confirm;
+
+            do
+            {
+                Console.WriteLine("Enter a digit to sum natural numbers up to that");
+                int n = int.Parse(Console.ReadLine());
+
+                int sum = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    sum += i;
+                }
+                Console.WriteLine($"The sum of {n} number is: {sum}");
+
+                Console.WriteLine("Do you want to continue type yes/no");
+                confirm = Console.ReadLine().ToLower();
+
+            }while( confirm == "yes" );
+            
+        }
+    }
+}
