c5eb83dc61285857a4a4fc9c05eb968a1e9d56af
Sayyab Khan <sayyabkhan007@gmail.com>
2023-09-08 05:40:18 -0700

    Solved Exercise 5

---
+++ ForLoop/Exe_5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    internal class Exe_5
+    {
+        //5. Write a C# Sharp program to display the cube of an integer up to given number.
+
+        public static void FindCube()
+        {
+            string confirm;
+            do
+            {
+                Console.WriteLine("Enter a digit");
+                int digit = int.Parse(Console.ReadLine());
+
+                double cube = 0;
+
+                for (int i = 1; i <= digit; i++)
+                {
+                    cube = Math.Pow(i, 3);
+                    Console.WriteLine($"Number is : {i} and cube of the {i} is : {cube}");
+                }
+
+                Console.WriteLine("Do you want to continue type yes/no");
+                confirm = Console.ReadLine();
+            }while (confirm == "yes");
+            
+
+        }
+    }
+}
