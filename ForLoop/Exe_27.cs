b0de0999a7623a0eef6a266c285097fbb5f2ce86
Sayyab Khan <sayyabkhan007@gmail.com>
2023-09-14 14:40:16 -0700

    Solved Excercise 27

---
+++ ForLoop/Exe_27.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    internal class Exe_27
+    {
+        //27. Write a C# Sharp program for checking if a number is a perfect number.
+
+        public static void CheckAPerfectNumber()
+        {
+            bool confirm = true;
+            while (confirm)
+            {
+                Console.WriteLine("Enter a number");
+                int num = int.Parse(Console.ReadLine());
+
+                int sum = 0;
+
+
+                for (int i = 1; i < num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        
+                        sum += i;
+                        Console.Write(i + " ");
+                    }
+                }
+                Console.WriteLine($"\nThe sum of the divisor is : {sum}");
+                if(sum == num)
+                {
+                    Console.WriteLine($"{num} is a perfect number");
+                }
+                else
+                {
+                    Console.WriteLine($"{num} is not a perfect number");
+                }
+
+
+
+
+                Console.WriteLine("Do you want to continue? yes/no");
+                string choice = Console.ReadLine();
+
+                if (choice == "yes")
+                {
+                    confirm = true;
+                }
+                else
+                {
+                    confirm = false;
+                }
+            }
+        }
+    }
+}
