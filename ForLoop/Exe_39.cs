08998f74e8fa31aa4e60b2a9891ae6327e186d29
Sayyab Khan <sayyabkhan007@gmail.com>
2023-09-16 15:02:34 -0700

    Solved Exercise 39

---
+++ ForLoop/Exe_39.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    internal class Exe_39
+    {
+        //39. Write a C# Sharp program to find the number and
+        //sum of all integers between 100 and 200 divisible by 9.
+
+        public static void FindNumsAndSum()
+        {
+
+                int sum = 0;
+                for (int i = 101; i < 200; i++)
+                {
+                    if(i % 9 == 0)
+                    {
+                        Console.Write(i + " ");
+                        sum += i;
+                    }
+                }
+                Console.WriteLine($"\nThe sum is = {sum}");
+
+
+
+
+
+            
+
+        }
+    }
+}
